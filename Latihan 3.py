1d9c31b2380c4b42dde1893f3d776b3f9d345a12
Varstzeiin <m.vitoariesta@gmail.com>
2023-03-04 07:39:20 +0700

    data termodifikasoi

--- Latihan 3.py
+++ Latihan 3.py
@@ -4,3 +4,10 @@ def luas_segitiga() :
     luas = alas*tinggi /2
     print ("Luas segitiga tersebut adalah = ",luas)
 luas_segitiga()
+
+def luas_persegi():
+    sisi1= int(input("masukkan sisi : "))
+    sisi2= int (input("masukkan sisi 2 : "))
+    luas = sisi1*sisi2
+    print("Luas persegi adalah =", luas)
+luas_persegi
